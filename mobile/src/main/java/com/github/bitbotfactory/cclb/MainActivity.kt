6c8b2550f9f632104fb3ecf530dd3831bea83c2f
Matt Mulhern <mattmulhern01@gmail.com>
2018-07-25 14:41:12 +0100

    slight changes, added last update value
    Signed-off-by: Matt Mulhern <mattmulhern01@gmail.com>

--- mobile/src/main/java/com/github/bitbotfactory/cclb/MainActivity.kt
+++ mobile/src/main/java/com/github/bitbotfactory/cclb/MainActivity.kt
@@ -3,25 +3,21 @@ package com.github.bitbotfactory.cclb
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.github.kittinunf.fuel.Fuel
+import android.widget.ListView
 import com.github.kittinunf.result.Result
 
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.TextView
-import com.beust.klaxon.JSON
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import com.beust.klaxon.string
-import com.github.kittinunf.fuel.android.extension.responseJson
-import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.getAs
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.json.JSONObject
+import android.widget.ArrayAdapter
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -35,15 +31,16 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Refreshing Data", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+
         }
 
     }
 
     fun updateView() {
         setContentView(R.layout.activity_main)
-        main_content_text
 
-        var url = "http://bitfinex.mulhernhex.duckdns.org:8045/botlog.json"
+
+        var url = "https://mulhernhex.duckdns.org/botlog/"
         url.httpGet().responseString { request, response, result ->
             //do something with response
             when (result) {
@@ -52,11 +49,15 @@ class MainActivity : AppCompatActivity() {
                 }
                 is Result.Success -> {
                     println("result: ${result}")
-                    var data_json = str2json(result.get()) as JsonObject
-                    var last_status = data_json.string("last_status")
-                    var main_txt = findViewById<TextView>(R.id.main_content_text)
-                    main_txt.setText(last_status)
+                    var dataJson = str2json(result.get()) as JsonObject
 
+                    var lastStatus = dataJson.string("last_status")
+                    var lastStatusTxt = findViewById<TextView>(R.id.last_status)
+                    lastStatusTxt.setText(lastStatus)
+
+                    var lastUpdate = dataJson.string("last_update")
+                    var lastUpdateTxt = findViewById<TextView>(R.id.last_update)
+                    lastUpdateTxt.setText(lastUpdate)
 
                 }
             }
@@ -65,11 +66,14 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+//    fun jsonList2str (input JsonArray) {
+//        for
+//
+//    }
     fun str2json(inStr: String) : JsonObject? {
         val parser: Parser = Parser()
         val stringBuilder: StringBuilder = StringBuilder(inStr)
         val parsed = parser.parse(stringBuilder) as JsonObject
-        println("MM: parsed: ${parsed}")
 
         return parsed
     }
